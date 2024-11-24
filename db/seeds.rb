62f713e88c341e8ab5ad18d328654cf56b3178f9
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-24 19:22:29 +0100

    chore(db): populate Location model with data from pogo.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,28 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'csv'
+
+csv_file_path = Rails.root.join('db', 'pogo.csv')
+
+CSV.foreach(csv_file_path, headers: true) do |row|
+years_data = {
+    "2017" => row['is_2017'] == 'true',  # Convert the string "true" to actual boolean true
+    "2018" => row['is_2018'] == 'true',
+    "2019" => row['is_2019'] == 'true',
+    "2020" => row['is_2020'] == 'true',
+    "2021" => row['is_2021'] == 'true',
+    "2022" => row['is_2022'] == 'true',
+    "2023" => row['is_2023'] == 'true',
+    "2024" => row['is_2024'] == 'true'
+  }
+
+  Location.create!(
+    name: row['name'],
+    latitude: row['latitude'].to_f,
+    longitude: row['longitude'].to_f,
+    image: row['img_url'],
+    description: row['description'],
+    address: row['address'],
+    years: years_data
+  )
+end
+
+puts "Locations generated successfully"
