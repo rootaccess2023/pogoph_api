9247e666a32875eb4022e72a7e5a873034e3c18f
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-24 19:24:22 +0100

    feat(controllers): define index action for Locations controller with year filter

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,14 @@
 class LocationsController < ApplicationController
+  def index
+    if params[:year].present?
+      year = params[:year]
+      @locations = Location.where("years ->> ? = ?", year, "true")
+    else
+      @locations = Location.all
+    end
+
+    render json: @locations
+  end
+end
+class LocationsController < ApplicationController
 end
