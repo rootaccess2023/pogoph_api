fadea6ef29818ed6d3cf40733e43d74219f3a009
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-26 18:56:12 +0100

    chore(db): run db:migrate to add region column to locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_24_182116) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_26_175434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,5 +24,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_24_182116) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
+    t.string "region"
   end
 end
